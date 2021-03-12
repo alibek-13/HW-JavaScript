9ed6fadffddc6be422527a2a363be9181770b7e1
Alibek Izmagambetov <alibekizmagambetov91@gmail>
2021-03-12 16:43:58 +0600

    finish up

--- HW-1-2/main.js
+++ HW-1-2/main.js
@@ -11,72 +11,20 @@
 // Вынести процесс логирования в консоль в отдельную функцию
 
 
-let balance = 0;
+let Box = 0;
 
-console.log("Ваш баланс: " + balance + "тг.");
+console.log("В ящике: " + Box + " яблок");
 
-if (balance > 0) {
-@@ -36,6 +35,8 @@ if (balance > 0) {
-}
+let apple = 5;
+// let isPositive =  apple > 0 ? "Положительное"  : "Не положительное";
 
-let a = "c";
-a = "b";
-a + 1;
+console.log( apple === 0 ? "Ноль" : (apple  > 0 ? "Положительное" : "Отрицательное"));
 
-if (true) {
-  a = "a";
-  console.log(a);
-}
-if (true) {
-  let a = "b";
-  console.log(a);
-}
-console.log(a);
+console.log( apple === 0 ? "Ноль" : (apple  > 0 ? "Положительное" : "Отрицательное"));
 
 
 
-let fruit = "апельсин"; // "апельсин", "груша"
-
-switch (fruit) {
-  case "яблоко":
-    console.log("Яблоко зеленое");
-    break;
-
-  case "апельсин":
-    console.log("Апельсин оранжевый");
-    break;
-
-  case "груша":
-    console.log("Груша желтая");
-    break;
-
-  default: 
-    console.log("Неизвестный фрукт");
-    break;
-}
-
-
-let number = -110;
-let isPositive = 
-  number > 0 
-  ? "Положительное" 
-  : "Не положительное";
-
-console.log(
-  number === 0 
-  ? "Ноль"
-  : (number > 0 
-    ? "Положительное" 
-    : "Отрицательное"
-  )
-);
-
-
-let negativeNumber = number * (
-  number > 0 
-  ? -1 
-  : 1
-);
+let negativeNumber = apple * (apple > 0 ? -1 : 1);
 // number * (??)
 // -110 * 1 = -110
 // 110 * -1 = -110
@@ -86,31 +34,62 @@ console.log(negativeNumber);
 
 // let var = условие ? выражение если условие верное : выражение если условие не верно
 
-// if (условие) {
-//  инструкции если условие верное 
-// } else {
-//  инструкции  если условие не верно
+// if (условие) { инструкции если условие верное}
+// else { инструкции  если условие не верно}
+
+// true false
+// 0 = false
+// остальные числа true
+// "" = false
+// остальные true
+
+// let apple = 5;
+//     apple = 10;
+//     apple = -15;
+
+// if (true) {
+//     apple = 5;
+//   console.log("В ящике: " + apple + " яблок");
+// }
+// if (true) {
+//   let a = "b";
+//   console.log(a);
+// }
+// console.log(apple);
+
+
+
+// let fruit = "груша"; // "апельсин", "груша"
+
+// switch (fruit) {
+//   case "яблоко":
+//     console.log("Яблоко зеленое");
+//     break;
+
+//   case "апельсин":
+//     console.log("Апельсин оранжевый");
+//     break;
+
+//   case "груша":
+//     console.log("Груша желтая");
+//     break;
+
+//   default: 
+//     console.log("Неизвестный фрукт");
+//     break;
 // }
 
 
 
-// true false
 
-// 0 = false
-// остальные числа true
+// let divisor = 10;
+// let divider = 40;
 
-// "" = false
-// остальные true
+// console.log(divider && divisor / divider);
 
+// let fullname = "Антон";
+// let age = "28";
 
-let divisor = 10;
-let divider = 40;
+// console.log("Имя: " + fullname + (age && " Возраст: " + age));
 
-console.log(divider && divisor / divider);
-
-let fullname = "Антон";
-let age = "28";
-
-console.log("Имя: " + fullname + (age && " Возраст: " + age));
-
-// 
+// // 
