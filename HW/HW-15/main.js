1465486580bcf6d1edbc58735e72a16b60dd1b37
Alibek <1_wolf_1@inbox.ru>
2021-04-10 14:51:17 +0600

    15

---
+++ HW/HW-15/main.js
@@ -0,0 +1,11 @@
+  
+const reverseStringRecursive = string => {
+    if (string.length === 1) { 
+      return string;
+    } 
+    else {
+      return reverseStringRecursive(string.slice(1)) + string[0];
+    }
+  }
+  
+console.log(reverseStringRecursive("Ghfd".toLowerCase()));
